d0edfc766aae13f6603aa9e5423e346765f60a0b
turpp <white.tripp@icloud.com>
2020-12-03 03:39:46 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,10 +21,12 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    # @students = Student.all.where("name like ?", params[:search])
+    @students=Student.search(params[:search])
   end
+  
 
   def student_params
-    params.require(:student).permit(:name, :birthday, :hometown)
+    params.require(:student).permit(:name, :birthday, :hometown, :search)
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search)
+    if search
+      Student.all.where("name LIKE '#{search}%'")
+    else
+      Student.all
+    end
+  
+  end
 end
